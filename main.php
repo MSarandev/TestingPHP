d1651ba3f1dcc38cd9338c1d9b4cb55b1631f818
1406519 <maxsarandev@gmail.com>
2016-10-03 15:40:57 +0100

    Prop submit 1

--- main.php
+++ main.php
@@ -17,12 +17,22 @@
     <h1 style='text-align: center; text-decoration: underline;'>Exercises Below</h1>
     <br>
     <!-- EXERSICE 1 -->
+    <div style="text-align: center; border: 1px solid black; border-radius: 50px;">
     <form method="post" action="">
         <h3>Enter you age below</h3>
         <input type="text" name="value">
         <input type="submit">
     </form>
+    </div>
     <br>
+    <!-- EXERSICE 2 -->
+    <div style="text-align: center; border: 1px solid black; border-radius: 50px;">
+        <form method="post" action="">
+            <h3>Enter the number of hobbits</h3>
+            <input type="text" name="hobbitsVal">
+            <input type="submit">
+        </form>
+    </div>
     <?php
     // Exercise 1
 
@@ -40,6 +50,24 @@
             echo "You are not allowed to buy anything";
         }
 
+    }
+
+    // Exercise 2
+
+    $numberOfHobbits = $_POST['hobbitsVal'];
+
+    switch($numberOfHobbits){
+        case 1:
+            echo "<strong>$numberOfHobbits</strong> Sad Hobbit";
+            break;
+        case 2:
+            echo "<strong>$numberOfHobbits</strong> Happy Hobbit";
+            break;
+        case 3:
+            echo "<strong>$numberOfHobbits</strong> Hobbits are a crowd";
+            break;
+        default:
+            echo "All Hobbits have gone home!";
     }
     ?>
 </body>
