ae0b366e57e96c3f57ea655211906df64b223f96
1406519 <maxsarandev@gmail.com>
2016-10-03 15:28:16 +0100

    Prop submit 1

--- main.php
+++ main.php
@@ -3,12 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <title>Lab1 Page</title>
+    <h1 style='text-align: center;'>Hello World - PHP</h1>
+    <br>
+    <h1 style='text-align: center; text-decoration: underline;'>Exercises Below</h1>
+    <br>
+    <!-- EXERSICE 1 -->
+    <form method="post" action="">
+        <h3>Enter you age below</h3>
+        <input type="text" name="value">
+        <input type="submit">
+    </form>
+    <br>
 </head>
 <body>
     <?php
-        echo "<h1 style='text-align: center;'>Hello World - PHP</h1>";
-        echo "<br>";
-        echo date('l jS \of F Y h:i:s A');
+    // Exercise 1
+
+    $myage = $_POST['value'];
+
+    if (intval($myage) >= 16){
+        echo "You are allowed to buy <strong>SPECS</strong>";
+    }else if(intval($myage) >= 18){
+        echo "You are allowed to buy <strong>MUGS</strong>";
+    }else if(intval($myage) >= 21){
+        echo "You are allowed to buy <strong>SAUSAGE ROLLS</strong>";
+    }else{
+        echo "You're not allowed to buy anything yet!";
+    }
     ?>
 </body>
 </html>
