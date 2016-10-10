6b703cfb97459d48690f05da58a84da4e40bf921
1406519 <maxsarandev@gmail.com>
2016-10-10 14:29:25 +0100

    Update --> (10/10/2016)

--- WK3/sql_generation.php
+++ WK3/sql_generation.php
@@ -28,8 +28,7 @@ include("db_connect.php"); // INCLUDE THE CONNECTION FILE
     $res = mysqli_query($db_conn, $sql_query);
     var_dump($res);
     // Fetch the result
-    $row = mysqli_fetch_row($res, MYSQLI_ASSOC);
-    print($row);
+    $row = mysqli_fetch_array($res, MYSQLI_ASSOC);
     ?>
 </div>
 </body>
