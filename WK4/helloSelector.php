ca6d4b4f28e56df8929a4d82af4aa6a8cce89dd3
1406519 <maxsarandev@gmail.com>
2016-10-17 13:44:19 +0100

    Update --> (10/10/2016)

--- WK4/helloSelector.php
+++ WK4/helloSelector.php
@@ -1,7 +1,36 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1406519
- * Date: 17/10/2016
- * Time: 13:08
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Group Work Dev page</title>
+    <style>
+        html, body {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+        #floatable{
+            width: 100%;
+            align: center;
+            margin: 0 auto;
+        }
+        .f_icon{
+            position: absolute;
+            float: left;
+            width: 200px;
+            height: 200px;
+        }
+    </style>
+</head>
+<body>
+<div id="main_cont">
+    <h2>Where would you like to say hello?</h2>
+    <br>
+    <div class="floatable">
+        <img class="f_icon" src="http://bestanimations.com/Earth&Space/Earth/earthglobeanimation/globe-earth-animation-6.gif"/>
+        <img class="f_icon" src="https://media.giphy.com/media/13cxHLqRer5q6s/giphy.gif"/>
+        <img class="f_icon" src="http://www.skymarvels.com/infopages/images/Uranus%20Orientation%20MED%20-%20SkyMarvels.gif"/>
+    </div>
+</div>
+</body>
+</html>
