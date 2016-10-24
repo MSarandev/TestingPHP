746844c689e70d7e18996796cfa3e94b9aaf59ac
1406519 <maxsarandev@gmail.com>
2016-10-24 15:00:19 +0100

    Week 5 - fix attempt 6

--- WK5/loggedin.php
+++ WK5/loggedin.php
@@ -4,6 +4,8 @@ $accessLVL = $_COOKIE['access_level_cookie'];
 
 echo "<p>Access Panel</p>";
 
+echo $accessLVL;
+
 function disInfo($accessLVL){
     if($accessLVL == 'standarduser'){
         echo "<p>1</p>";
