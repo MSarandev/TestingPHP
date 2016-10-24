21cc2eb56bebd93aa46b45611609370908fe6600
1406519 <maxsarandev@gmail.com>
2016-10-24 14:43:08 +0100

    Week 5 - fix attempt 1

--- WK5/checklogin.php
+++ WK5/checklogin.php
@@ -4,7 +4,7 @@ $user = $_POST["txt_user"];
 $pass = $_POST["txt_pass"];
 
 if($user == "username" && $pass == "password"){
-    setcookie('access_level_cookie', 'standard_user');
+    setcookie('access_level_cookie', 'standarduser');
 }
 
 header('Location : loggedin.php');
