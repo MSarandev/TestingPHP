f7e519e44e9cf018f2bd9cd25ae1fc658865d0ca
1406519 <maxsarandev@gmail.com>
2016-10-31 15:58:19 +0000

    Week 6 -  First try for creation (3.7.2)

--- WK6/index.php
+++ WK6/index.php
@@ -4,25 +4,32 @@ $rules = array(
     //
     //main pages
     //
-    'about' => "/about.php",
-    'contactus' => "/contactus.php",
-    'blog' => "/blog.php",
+    'about' => "/about",
+    'contactus' => "/contactus",
+    'blog' => "/blog",
     //'blog_article' => "/blog/(?'blogID'[\w\-]+)",
     //
     //Admin Pages
     //
-    'login' => "/login.php",
-    'create_article' => "/create_article.php",
-    'logout' => "/logout.php",
+    'login' => "/login",
+    'create_article' => "/createarticle",
+    'logout' => "/logout",
     //
     // Home Page
     //
-    'home' => "/home.php"
+    'home' => "/home"
 );
 $uri = rtrim(dirname($_SERVER["SCRIPT_NAME"]), '/');
 $uri = '/' . trim(str_replace($uri,
         ''
         , $_SERVER['REQUEST_URI']), '/');
+$uri = urldecode($uri);
+foreach ($rules as $action => $rule) {
+    if (preg_match('~^' . $rule . '$~i', $uri, $params)) {
+        include(INCLUDE_DIR . $action . '.php');
+        exit();
+    }
+}
 // nothing is found so handle the 404 error
-//include(INCLUDE_DIR . '/404.php');
+include(INCLUDE_DIR . '/404.php');
 ?>
