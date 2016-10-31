876dad663d5f5860272e89b764d594c617f4d9fb
1406519 <maxsarandev@gmail.com>
2016-10-31 15:37:29 +0000

    Week 6 -  First try for creation (3.2)

---
+++ WK6/scripts/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>The Blog of Charles Babbage</title>
+</head>
+<body>
+<header>
+    <h1>The Blog of Charles Babbage</h1>
+    <nav>
+        <ul>
+            <li><a href="./">Home Page</a></li>
+            <li><a href="blog.php">My Blog</a></li>
+            <li><a href="about.php">About Me</a></li>
+            <li><a href="contactus">Contact Me</a></li>
+            <?php
+            ini_set('display_errors', 1);
+            ini_set('display_startup_errors', 1);
+            error_reporting(E_ALL);
+
+            if (isset($_SESSION['username'])) {
+                echo "<li><a href='create_article'>Create Article</a></li>";
+                echo "<li><a href='logout'>Logout</a></li>";
+            } else {
+                echo "<li><a href='login'>Login</a></li>";
+            }
+            ?>
+        </ul>
+    </nav>
+</header>
